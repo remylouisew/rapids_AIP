6122f16a55b99c03a71621bd52dc35be145e4209
remy <remylouisew@gmail.com>
2021-01-27 19:28:10 +0000

    new yamls

--- gcsfs_localcuda/entrypoint.sh
+++ gcsfs_localcuda/entrypoint.sh
@@ -1,5 +1,5 @@
 source /conda/etc/profile.d/conda.sh
 conda activate rapids
 
-echo "Running: rapids_opt2.py $@"
-python rapids_opt2.py $@
+echo "Running: rapids_xgboost.py $@"
+python rapids_xgboost.py $@
