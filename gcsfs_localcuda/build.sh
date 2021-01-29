c6bf35b0b90ad22d2813cc2599d4b1a662b76779
remy <remylouisew@gmail.com>
2021-01-29 21:33:07 +0000

    com

--- gcsfs_localcuda/build.sh
+++ gcsfs_localcuda/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Build the Rapids-based custom container with the XGboost training code
+## Change the gcr.io path to your own repository!
 
 docker build -t gcr.io/remy-sandbox/rapids_gcsfs:latest .
 
